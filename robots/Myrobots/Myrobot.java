3ee4ccd519486b6c5f6875c008bb0b8210341673
13510103 <13510103@std.stei.itb.ac.id>
2013-10-03 13:55:11 +0700

    myrobot

---
+++ robots/Myrobots/Myrobot.java
@@ -0,0 +1,60 @@
+package Myrobots;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * Myrobot - a robot by (your name here)
+ */
+public class Myrobot extends Robot
+{
+	/**
+	 * run: Myrobot's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			//ahead(100);
+			//turnGunRight(360);
+			//back(100);
+			//turnGunRight(360);
+			turnRadarRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		setAdjustRadarForRobotTurn(true);
+		//turnRadarRight(getHeading() - getRadarHeading() + e.getBearing());
+		//turnGunRight(getRadarHeading());
+		fire(3);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
